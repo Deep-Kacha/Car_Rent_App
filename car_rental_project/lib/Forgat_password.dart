2a7ccc8b4713acc11cd0cee1c8548e2bb32f1516
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-08-15 18:42:47 +0530

    login and other page

---
+++ car_rental_project/lib/Forgat_password.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ForgatPassword extends StatefulWidget {
+  const ForgatPassword({super.key});
+
+  @override
+  State<ForgatPassword> createState() => _ForgatPasswordState();
+}
+
+class _ForgatPasswordState extends State<ForgatPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
