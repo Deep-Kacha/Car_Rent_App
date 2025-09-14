83e841e8c37b2109be44a319c3ae48af139e223f
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-09-14 18:52:23 +0530

    create favorite page to book car

--- car_rental_project/lib/Authorization/Favorite_car/Favorite.dart
+++ car_rental_project/lib/Authorization/Favorite_car/Favorite.dart
@@ -1,3 +1,4 @@
+import 'package:car_rental_project/Authorization/Booking/Book_car.dart';
 import 'package:car_rental_project/Authorization/Home%20Page/car_model.dart';
 import 'package:flutter/material.dart';
 
@@ -141,7 +142,13 @@ class FavoritePage extends StatelessWidget {
                                             ),
                                           ),
                                           onPressed: () {
-                                            // Book action
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    BookingPage(car: car),
+                                              ),
+                                            );
                                           },
                                           child: Text(
                                             car.price,
