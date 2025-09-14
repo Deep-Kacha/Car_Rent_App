a34fc8af754d19dd7fdd2dce66ac838b59441292
Deep-Kacha <deepnkacha2256@gmail.com>
2025-09-14 21:58:53 +0530

    Set A Profile Image Network image to AssetsImage

---
+++ car_rental_project/lib/Authorization/Home Page/home_page.dart
@@ -0,0 +1,382 @@
+import 'package:car_rental_project/Authorization/Booking/Book_car.dart';
+import 'package:car_rental_project/Authorization/Favorite_car/Favorite.dart';
+import 'package:car_rental_project/Authorization/Menu/Menu.dart';
+import 'package:flutter/material.dart';
+import 'car_model.dart';
+import 'car_data.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+  String selectedCategory = "All";
+  String searchQuery = "";
+  Set<String> favoriteCars = {};
+
+  final List<String> categories = ["All", "Cars", "SUVs", "XUVs", "Vans"];
+  void onToggleFavorite(Car car) {
+    setState(() {
+      if (favoriteCars.contains(car.name)) {
+        favoriteCars.remove(car.name);
+      } else {
+        favoriteCars.add(car.name);
+      }
+    });
+  }
+
+  List<Car> get filteredCars {
+    final carsByCategory = selectedCategory == "All"
+        ? cars
+        : cars.where((car) => car.category == selectedCategory).toList();
+
+    if (searchQuery.isEmpty) return carsByCategory;
+
+    return carsByCategory
+        .where(
+          (car) => car.name.toLowerCase().contains(searchQuery.toLowerCase()),
+        )
+        .toList();
+  }
+
+  Widget buildNavItem(IconData icon, String label, int index) {
+    bool isSelected = selectedIndex == index;
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(icon, size: 25, color: isSelected ? Colors.black : Colors.grey),
+          SizedBox(height: 6),
+          Text(
+            label,
+            style: TextStyle(
+              color: isSelected ? Colors.black : Colors.grey,
+              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// ✅ Home Page
+  Widget buildHomePage() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        /// Profile + Search
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 21,
+                    backgroundImage: AssetImage("assets/images/profile.jpg"),
+                  ),
+                  SizedBox(width: 15),
+                  Text(
+                    "Ethan John",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Search cars near you...",
+                  prefixIcon: Icon(Icons.search),
+                  filled: true,
+                  fillColor: Color(0xFFF5F5F5),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    searchQuery = value;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+
+        /// Categories
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            "Categories",
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          height: 40,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            itemCount: categories.length,
+            itemBuilder: (context, index) {
+              final category = categories[index];
+              final isSelected = selectedCategory == category;
+
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedCategory = category;
+                  });
+                },
+                child: Container(
+                  margin: EdgeInsets.only(right: 20),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: isSelected ? 30 : 16,
+                    vertical: 8,
+                  ),
+                  decoration: isSelected
+                      ? BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(30),
+                        )
+                      : null,
+                  alignment: Alignment.center,
+                  child: Text(
+                    category,
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: isSelected
+                          ? FontWeight.bold
+                          : FontWeight.normal,
+                      color: isSelected ? Colors.white : Colors.grey,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+
+        SizedBox(height: 10),
+
+        /// Cars List
+        Expanded(
+          child: filteredCars.isEmpty
+              ? Center(
+                  child: Text(
+                    "No cars available in this category",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                )
+              : ListView.builder(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  itemCount: filteredCars.length,
+                  itemBuilder: (context, index) {
+                    final car = filteredCars[index];
+                    final isFavorite = favoriteCars.contains(car.name);
+
+                    return Container(
+                      margin: EdgeInsets.only(bottom: 16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 5,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.vertical(
+                              top: Radius.circular(15),
+                            ),
+                            child: Image.asset(
+                              car.image,
+                              height: 180,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Color(0xFF3E2723),
+                              borderRadius: BorderRadius.vertical(
+                                bottom: Radius.circular(15),
+                              ),
+                            ),
+                            padding: EdgeInsets.all(12),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                /// Car Name + Favorite
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      car.name,
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          if (isFavorite) {
+                                            favoriteCars.remove(car.name);
+                                          } else {
+                                            favoriteCars.add(car.name);
+                                          }
+                                        });
+                                      },
+                                      child: Icon(
+                                        isFavorite
+                                            ? Icons.favorite
+                                            : Icons.favorite_border,
+                                        color: Colors.redAccent,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                                SizedBox(height: 4),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        car.address,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ),
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.orange,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(
+                                            8,
+                                          ),
+                                        ),
+                                        padding: EdgeInsets.symmetric(
+                                          horizontal: 14,
+                                          vertical: 6,
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                BookingPage(car: car),
+                                          ),
+                                        );
+                                      },
+                                      child: Text(
+                                        car.price,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+        ),
+      ],
+    );
+  }
+
+  /// ✅ Favorites Page
+  Widget buildFavoritesPage() {
+    // get Car objects whose names are in favoriteCars
+    final favoriteList = cars
+        .where((car) => favoriteCars.contains(car.name))
+        .toList();
+
+    return FavoritePage(
+      favoriteCars: favoriteList,
+      onToggleFavorite: onToggleFavorite,
+    );
+  }
+
+  /// ✅ Menu Page
+  Widget buildMenuPage() {
+    return MenuPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: selectedIndex == 0
+            ? buildHomePage()
+            : selectedIndex == 1
+            ? Center(child: Text("Booked Page")) // You can design this later
+            : selectedIndex == 2
+            ? buildFavoritesPage()
+            : buildMenuPage(),
+      ),
+
+      /// Bottom Nav
+      bottomNavigationBar: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 5,
+              offset: Offset(0, -3),
+            ),
+          ],
+        ),
+        padding: EdgeInsets.symmetric(vertical: 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            buildNavItem(Icons.home, "Home", 0),
+            buildNavItem(Icons.directions_car, "Booked", 1),
+            buildNavItem(Icons.favorite_border, "Favorite", 2),
+            buildNavItem(Icons.menu, "Menu", 3),
+          ],
+        ),
+      ),
+    );
+  }
+}
