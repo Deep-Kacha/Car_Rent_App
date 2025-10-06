92dc071098c4920f9ade260ef88d3595b883c7b1
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-10-06 16:21:48 +0530

    changes in pubspeck yaml files

--- car_rental_project/lib/main.dart
+++ car_rental_project/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:car_rental_project/Admin%20Panel%20Files/Done.dart';
+// import 'package:car_rental_project/Admin%20Panel%20Files/Done.dart';
 import 'package:car_rental_project/Authorization/Home%20Page/home_page.dart';
 // import 'package:car_rental_project/Menu/Account.dart';
 
